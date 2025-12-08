b6f7ed040cf38d89f66d9845ff329f9be0b613cb
Cory Frenette <cory@frenette.dev>
2025-12-07 23:06:46 -0500

    day 4, part 2

--- day-04-printing-department/src/main.rs
+++ day-04-printing-department/src/main.rs
@@ -1,13 +1,14 @@
 use aoc_2025::read_input_by_line;
 
 fn main() {
-    if let Ok(mut lines) = read_input_by_line("example") {
+    if let Ok(mut lines) = read_input_by_line("input") {
         let size = lines.size_hint().1.unwrap_or(lines.size_hint().0);
         let mut map = Vec::with_capacity(size);
         while let Some(Ok(line)) = lines.next() {
             map.push(line.chars().map(|c| c == '@').collect());
         }
         part_one(&map);
+        part_two(&mut map);
     }
 }
 
@@ -75,4 +76,85 @@ fn part_one(map: &Vec<Vec<bool>>) {
     }
 
     println!("Part one: {}", can_be_accessed);
+}
+
+fn part_two(map: &mut Vec<Vec<bool>>) {
+    let (rows, cols) = (map.len(), map.first().unwrap().len());
+    let mut num_free_positions = Vec::with_capacity(rows);
+    // Initialize, setting the free positions due to the borders
+    let mut first = vec![3u8; cols];
+    first[0] = 5;
+    first[cols - 1] = 5;
+    let last = first.clone();
+    num_free_positions.push(first);
+    for _row_num in 1..rows - 1 {
+        let mut row = vec![0; cols];
+        row[0] = 3;
+        row[cols - 1] = 3;
+        num_free_positions.push(row);
+    }
+    num_free_positions.push(last);
+
+    for (row_num, row) in map.iter().enumerate() {
+        for (col_num, col) in row.iter().enumerate() {
+            // If the position does not contain a paper roll,
+            // add a free position to each of its neighbors
+            if !col {
+                update_free_positions(&mut num_free_positions, row_num, col_num);
+            }
+        }
+    }
+
+    let mut total_removed = 0;
+    let mut removed_rolls = true;
+
+    while removed_rolls {
+        removed_rolls = false;
+        // remove rolls until no more can be removed
+        for (row_num, row) in map.iter_mut().enumerate() {
+            for (col_num, col) in row.iter_mut().enumerate() {
+                if *col {
+                    if num_free_positions[row_num][col_num] > 4 {
+                        if removed_rolls == false {
+                            removed_rolls = true;
+                        }
+                        *col = false;
+                        total_removed += 1;
+                        update_free_positions(&mut num_free_positions, row_num, col_num);
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Part two: {}", total_removed);
+}
+
+fn update_free_positions(num_free_positions: &mut Vec<Vec<u8>>, row_num: usize, col_num: usize) {
+    let rows = num_free_positions.len();
+    let cols = num_free_positions.first().unwrap().len();
+    if row_num > 0 {
+        if col_num > 0 {
+            num_free_positions[row_num - 1][col_num - 1] += 1;
+        }
+        num_free_positions[row_num - 1][col_num] += 1;
+        if col_num < cols - 1 {
+            num_free_positions[row_num - 1][col_num + 1] += 1;
+        }
+    }
+    if row_num < rows - 1 {
+        if col_num > 0 {
+            num_free_positions[row_num + 1][col_num - 1] += 1;
+        }
+        num_free_positions[row_num + 1][col_num] += 1;
+        if col_num < cols - 1 {
+            num_free_positions[row_num + 1][col_num + 1] += 1;
+        }
+    }
+    if col_num > 0 {
+        num_free_positions[row_num][col_num - 1] += 1;
+    }
+    if col_num < cols - 1 {
+        num_free_positions[row_num][col_num + 1] += 1;
+    }
 }
